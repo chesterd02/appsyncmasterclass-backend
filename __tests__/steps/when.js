dc32a71765a8d8ff28db5d8626ae4856d51cd222
ChesterD02 <chesterd02@gmail.com>
2021-01-22 11:23:44 -0700

    added e2e test for getTweets

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -151,6 +151,11 @@ const a_user_calls_tweet = async (user, text) => {
   const tweet = `mutation tweet($text: String!) {
     tweet(text: $text) {
       id
+      profile {
+        id 
+        name
+        screenName
+      }
       createdAt
       text
       replies
@@ -180,6 +185,42 @@ const we_invoke_an_appsync_template = (templatePath, context) => {
   return JSON.parse(compiler.render(context))
 }
 
+const a_user_calls_getTweets = async (user, userId, limit, nextToken) => {
+  const getTweets = `query getTweets($userId: ID!, $limit: Int!, $nextToken: String) {
+    getTweets(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      nextToken
+      tweets {
+        id
+        createdAt
+        profile {
+          id 
+          name
+          screenName
+        }
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+        }
+      }
+    }
+  }`
+  const variables = {
+    userId,
+    limit,
+    nextToken
+  }
+
+  const data = await GraphQL(process.env.API_URL, getTweets, variables, user.accessToken)
+  const newTweet = data.getTweets
+
+  console.log(`[${user.username}] - posted new tweet`)
+
+  return newTweet
+}
+
 module.exports = {
     we_invoke_confirmUserSignup,
     a_user_signs_up,
@@ -188,5 +229,6 @@ module.exports = {
     a_user_calls_getImageUploadUrl,
     we_invoke_tweet,
     a_user_calls_tweet,
-    we_invoke_an_appsync_template
+    we_invoke_an_appsync_template,
+    a_user_calls_getTweets
 }
